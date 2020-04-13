c70db68d3638027184cf2e4136c95cba97f854dd
mkmozgawa <hello@magda.space>
2020-04-13 16:09:18 +0200

    Update to 0.3

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## moneyprinter
+## moneyprintergobrrr
+
+[![PyPI version](https://badge.fury.io/py/moneyprintergobrrr.svg)](https://pypi.org/project/moneyprintergobrrr/)
 
 Based on [this meme](https://knowyourmeme.com/memes/money-printer-go-brrr) and specifically this image:
 
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.md') as readme_file:
     README = readme_file.read()
 
 setup_args = dict(
-    name='moneyprinter',
-    version='0.1',
+    name='moneyprintergobrrr',
+    version='0.3',
     description='Money printer go brr',
     long_description_content_type="text/markdown",
     long_description=README,
@@ -14,8 +14,8 @@ setup_args = dict(
     author='Magda Mozgawa',
     author_email="mkmozgawa@gmail.com",
     keywords=['Moneyprinter', 'Money printer', 'brrr', 'meme'],
-    url='https://github.com/mkmozgawa/moneyprinter',
-    download_url='https://pypi.org/project/moneyprinter/'
+    url='https://github.com/mkmozgawa/moneyprintergobrrr',
+    download_url='https://pypi.org/project/moneyprintergobrrr/'
 )
 
 if __name__ == '__main__':
